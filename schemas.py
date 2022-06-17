40185b576e38ba2e54166c748b145ee5116d4214
yolobanana <dimdimdimas@apps.ipb.ac.id>
2022-06-17 22:34:32 +0700

    fixing

--- schemas.py
+++ schemas.py
@@ -1,9 +1,6 @@
 from datetime import datetime
 from typing import List, Optional
-from unicodedata import name
 from pydantic import BaseModel
-from pyrsistent import s
-from sqlalchemy import Date
 
 class BookBase(BaseModel):
     isbn: str
